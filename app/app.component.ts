9d51ed93e848e21a9006a836d8c7d70c61e5ef16
rafysanchez <rafysanchez@hotmail.com>
2018-07-23 16:59:23 -0300

    adjs
    adjs

--- app/app.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id.toString(),
-    selector: 'app',
-    templateUrl: 'app.component.html'
-})
-
-export class AppComponent { }
---
+++ src/app/app.routing.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+var index_1 = require("./home/index");
+var index_2 = require("./test-page/index");
+var appRoutes = [
+    { path: '', component: index_1.HomeComponent },
+    { path: 'test-page', component: index_2.TestPageComponent },
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
